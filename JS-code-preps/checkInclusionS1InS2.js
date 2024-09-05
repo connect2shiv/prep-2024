d139f3b52974af59c48bddf43359c475a399a32a
Shivaprasad <connect2shiv@gmail.com>
2024-09-05 18:52:50 +0530

    Update checkInclusionS1InS2.js

--- JS-code-preps/checkInclusionS1InS2.js
+++ JS-code-preps/checkInclusionS1InS2.js
@@ -5,26 +5,24 @@
  */
 const getCharIndex = (ch) => ch.charCodeAt(0) - 'a'.charCodeAt(0);
 
-var checkInclusion = function(s1, s2) {    
+var checkInclusion = function(s1, s2) {
+    // Frequency array to char counts
     let map = Array(26).fill(0);
+
+    // count the char frequency of s1
     for (let ch of s1) map[getCharIndex(ch)]++;
 
-    let charCount = s1.length;
-    let i = 0, j = 0;
-    while (j < s2.length) {
-        if (map[getCharIndex(s2[j])] > 0)
-            charCount--;
-
-        map[getCharIndex(s2[j])]--;
-        j++;
+    let charCount = s1.length, start = 0;
+    for (let end = 0; end < s2.length; end++) {
+        if (map[getCharIndex(s2[end])] > 0) charCount--;
+        map[getCharIndex(s2[end])]--;
 
         if (charCount === 0) return true;
 
-        if (j - i === s1.length) {
-            if (map[getCharIndex(s2[i])] >= 0)
-                charCount++;
-            map[getCharIndex(s2[i])]++;
-            i++;
+        while (end - start + 1 === s1.length) {
+            if (map[getCharIndex(s2[start])] >= 0) charCount++;
+            map[getCharIndex(s2[start])]++;
+            start++;
         }
     }
     return false;
